6e0364dd17362e8a11917e31c46c269f92ccc877
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-19 14:44:41 +0100

    light changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -31,11 +31,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
 
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        UserProfile.objects.create(user=instance, role='Member')
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance, role='Member')
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,13 +44,13 @@ class LibraryDetailView(DetailView):
 
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.UserProfile.role == 'Admin'
 
 def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+    return user.UserProfile.role == 'Librarian'
 
 def is_member(user):
-    return user.userprofile.role == 'Member'
+    return user.UserProfile.role == 'Member'
 
 @user_passes_test(is_admin)
 def admin_view(request):
