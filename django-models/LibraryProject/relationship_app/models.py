feaa2a42c2eca73620867c85ace8496b8e7508da
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-19 13:33:22 +0100

    new files added for the new task

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save  
 
 # Create your models here.
 class Author(models.Model):
@@ -18,3 +21,21 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length = 200)
     library = models.OneToOneField(Library, on_delete = models.CASCADE)
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    ]
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            UserProfile.objects.create(user=instance, role='Member')
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.userprofile.save()
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy 
+from django.contrib.auth.decorators import user_passes_test 
 # Create your views here.
 
 
@@ -39,5 +40,27 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template =  'relationship_app/library_detail.html'
+
+
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
 
 
