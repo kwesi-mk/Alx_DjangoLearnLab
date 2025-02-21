868dcc7412a0c479f656369fac2bbdecd8b41452
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 08:47:33 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -47,7 +47,7 @@ class LibraryDetailView(DetailView):
     template =  'relationship_app/library_detail.html'
 
 def is_admin(user):
-    return user.role == 'Admin'
+    return user.userprofile.role == 'Admin'
 
 
 @user_passes_test(is_admin)
