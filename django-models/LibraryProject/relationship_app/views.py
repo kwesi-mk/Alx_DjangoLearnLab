ebb3e4a0da8f79f30d059b2ff286cad225fb2806
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 22:24:21 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -46,50 +46,50 @@ class LibraryDetailView(DetailView):
      model = Library
      template =  'relationship_app/library_detail.html'
 
-# def is_Admin(user):
-#     return user.userprofile.role == 'Admin'
-
-# def is_Member(user):
-#     return user.userprofile.role == 'Member'
-
-# def is_Librarian(user):
-#     return user.userprofile.role == 'Librarian'
-
-# @user_passes_test(is_Admin)
-# def Admin(request):
-#     return render(request, 'admin_view.html')
-
-# @user_passes_test(is_Librarian)
-# def Librarian(request):
-#     return render(request, 'librarian_view.html')
-
-# @user_passes_test(is_Member)
-# def Member(request):
-#     return render(request, 'member_view.html')
-
 def is_admin(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+    return user.userprofile.role == 'Admin'
 
 def is_member(user):
-    return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+    return user.userprofile.role == 'Member'
 
-#Admin view
-@login_required
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
 @user_passes_test(is_admin)
 def admin_view(request):
-    return render(request, "relationship_app/admin_view.html", {"role":"Admin"})
+    return render(request, 'relationship_app/admin_view.html')
 
-#Librarian View
-@login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return render(request, "relationship_app/librarian_view.html", {"role": "Librarian"})
-    
-#Member View
-@login_required
+    return render(request, 'relationship_app/librarian_view.html')
+
 @user_passes_test(is_member)
 def member_view(request):
-    return render(request, "relationship_app/member_view.html", {"role":"Member"})
+    return render(request, 'relationship_app/member_view.html')
+
+# def is_admin(user):
+#     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+# def is_librarian(user):
+#     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+# def is_member(user):
+#     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+# #Admin view
+# @login_required
+# @user_passes_test(is_admin)
+# def admin_view(request):
+#     return render(request, "relationship_app/admin_view.html", {"role":"Admin"})
+
+# #Librarian View
+# @login_required
+# @user_passes_test(is_librarian)
+# def librarian_view(request):
+#     return render(request, "relationship_app/librarian_view.html", {"role": "Librarian"})
+    
+# #Member View
+# @login_required
+# @user_passes_test(is_member)
+# def member_view(request):
+#     return render(request, "relationship_app/member_view.html", {"role":"Member"})
