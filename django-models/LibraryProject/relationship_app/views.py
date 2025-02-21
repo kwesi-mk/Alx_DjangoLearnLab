171937adf6c661d8e56f10623f8a3ab0c30153c4
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 22:39:32 +0100

    new changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,7 +11,26 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test, login_required 
 # Create your views here.
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
 
 
 def register(request):
@@ -42,32 +61,15 @@ def list_books(request):
 
 
 
-class LibraryDetailView(DetailView):
-     model = Library
-     template =  'relationship_app/library_detail.html'
-
-def is_admin(user):
-    return user.userprofile.role == 'Admin'
-
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
-
-def is_member(user):
-    return user.userprofile.role == 'Member'
+# class LibraryDetailView(DetailView):
+#      model = Library
+#      template =  'relationship_app/library_detail.html'
 
 
 
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, 'relationship_app/admin_view.html')
 
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, 'relationship_app/librarian_view.html')
 
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, 'relationship_app/member_view.html')
+
 
 # def is_admin(user):
 #     return user.is_authenticated and hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
