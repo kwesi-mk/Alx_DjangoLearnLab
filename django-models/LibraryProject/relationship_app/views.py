c3bae74b2f4807b00d911802896d5b5dcd36906c
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 08:44:04 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -11,13 +11,7 @@ from django.urls import reverse_lazy
 from django.contrib.auth.decorators import user_passes_test 
 # Create your views here.
 
-def is_admin(user):
-    return user.role == 'Admin'
 
-
-@user_passes_test(is_admin)
-def Admin(request):
-    return render(request, 'admin_view.html')
 
 
 def register(request):
@@ -46,16 +40,19 @@ def list_books(request):
         'books' : books 
     })
 
+
+
 class LibraryDetailView(DetailView):
     model = Library
     template =  'relationship_app/library_detail.html'
 
+def is_admin(user):
+    return user.role == 'Admin'
 
 
-
-
-
-
+@user_passes_test(is_admin)
+def Admin(request):
+    return render(request, 'admin_view.html')
 
 
 def is_librarian(user):
