b8b81c5e509c2d070774e3e1a6d10519020fe198
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 08:39:35 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views 
+from . import views
+ 
 
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, Admin_view, librarian_view, member_view 
 
 app_name = 'relationship_app'
 
@@ -13,7 +14,7 @@ urlpatterns = [
     #path('register/', SignUpView.as_view(template_name='relationship_app/register.html'), name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('admin-view/', admin_view, name='admin_view'),
+    path('admin-view/', Admin_view, name='admin_view'),
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,7 +16,7 @@ def is_admin(user):
 
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin_view(request):
     return render(request, 'admin_view.html')
 
 
