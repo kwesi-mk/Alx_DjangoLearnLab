cd125eeb5e23141b0c2a132700ecfa070046467a
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 08:49:54 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from .models import UserProfile
+from .models import UserProfile 
 from django.shortcuts import render, redirect  
 from django.views.generic.detail import DetailView 
 from django.contrib.auth import login
@@ -47,7 +47,7 @@ class LibraryDetailView(DetailView):
     template =  'relationship_app/library_detail.html'
 
 def is_admin(user):
-    return user.userprofile.role == 'Admin'
+    return user.UserProfile.role == 'Admin'
 
 
 @user_passes_test(is_admin)
