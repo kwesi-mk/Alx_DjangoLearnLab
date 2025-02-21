c631ea1c483cbdd00359b244308e037936c3fb00
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-21 22:32:26 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -49,11 +49,13 @@ class LibraryDetailView(DetailView):
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
 def is_member(user):
     return user.userprofile.role == 'Member'
 
-def is_librarian(user):
-    return user.userprofile.role == 'Librarian'
+
 
 @user_passes_test(is_admin)
 def admin_view(request):
