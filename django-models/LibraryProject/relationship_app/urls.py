8a6162d6d2f2337b39b892b401059471d7c872ae
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-22 11:17:09 +0100

    new changes added

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,13 +8,14 @@ from .views import list_books, LibraryDetailView, admin_view, librarian_view, me
 app_name = 'relationship_app'
 
 urlpatterns = [
+    path("admin/", views.admin_view, name="admin-view"),
+    path("librarian/", views.librarian_view, name="librarian-view"),
+    path("member/", views.member_view, name="member-view"), 
     path('', views.all_books, name='books'),
     path('', views.LibraryView.as_view(), name = 'library_view'),
     path('register/', views.register, name='register'),
     #path('register/', SignUpView.as_view(template_name='relationship_app/register.html'), name='register'),
     path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-    path("admin/", views.admin_view, name="admin-view"),
-    path("librarian/", views.librarian_view, name="librarian-view"),
-    path("member/", views.member_view, name="member-view"),
+    
 ]
