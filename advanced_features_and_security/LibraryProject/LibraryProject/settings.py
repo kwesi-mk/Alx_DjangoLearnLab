d89dab007d122bda0c9f1800185acb720d0c71c3
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-26 15:21:08 +0100

    modified the settings.py file

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = "accounts.CustomUser"
+AUTH_USER_MODEL = "bookshelf.CustomUser"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
