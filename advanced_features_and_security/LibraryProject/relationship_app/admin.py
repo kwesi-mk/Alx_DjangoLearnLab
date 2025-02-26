e25f08340579e02b7d2fadb04219da907267c26b
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-26 15:26:39 +0100

    modified the views.py file in bookshelf

--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -10,25 +10,5 @@ admin.site.register(Book)
 admin.site.register(Library)
 admin.site.register(Librarian)
 
-class ModelAdmin(UserAdmin):
-    """Admin panel customization for Customer"""
-    model = CustomUser
-    list_display = ("email", "first_name", "last_name", "date_of_birth", "is_staff", "is_superuser")
-    list_filter = ("is_staff", "is_superuser", "date_of_birth")
-    fieldsets = (
-        (None, {"fields": ("email", "passwords")}),
-        ("Personal Info", {"fields": ("first_name", "last_name", "date_of_birth", "profile_photo")}),
-        ("Permissions", {"fields": ("is_staff", "is_superuser", "is_active", "groups", "user_permissions")}),
-        ("Important dates", {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (None, {
-            "classes": ('wide',),
-            "fields": ("email", "first_name", "last_name", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_superuser"),
-        }),
-    )
-    search_fields = ("email","first_name", "last_name")
-    ordering = ("email",)
 
-admin.site.register(CustomUser, CustomUserAdmin)
 
