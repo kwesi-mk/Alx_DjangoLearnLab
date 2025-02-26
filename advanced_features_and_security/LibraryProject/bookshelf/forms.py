8bde85a1205013b99d0554e0b2b4727c9fb44559
Malachy K. Madu <kidakwesta9@gmail.com>
2025-02-26 16:31:26 +0100

    new changes in the views.py file

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Book
+
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'publication_date']
+        widgets = {
+            'publication_date': forms.DateInput(attrs={'type': 'date'})
+        }
