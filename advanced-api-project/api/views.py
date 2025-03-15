22c418298adcaa349f919b1989a4a54e6772f533
Malachy K. Madu <kidakwesta9@gmail.com>
2025-03-15 05:25:43 +0100

    first commit for the new task

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Book, Author
+from .serializers import BookSerializer, AuthorSerializer
 
 # Create your views here.
+class CustomBookCreateView(generics.CreateAPIView):
+    """Create a new book. Restricted to authenticated users."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """Customize behaviour before saving the object."""
+        serializers.save()
+
+class CustomBookListView(generics.ListAPIView):
+    """Retrieve all books."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookDetailView(generics.DetailAPIView):
+    """Retrieves a single book by ID"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookUpdateView(generics.UpdateAPIView):
+    """Update an existing book. Restricted to authenticated users."""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer 
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        """Customize behaviour before updating an object."""
+        serializer.save()
+
+class CustomBookDeleteView(generics.DestroyAPIView):
+    """Delete a book. Restricted to authenticated users."""
+    queryset = Book.objects.all()
+    seriailizer_class = BookSerializer 
+    permission_classes = [permissions.IsAuthenticated]
