218a5934a31d24f6fb3650bef8825c6107576576
Malachy K. Madu <kidakwesta9@gmail.com>
2025-03-27 12:11:14 +0100

    created a new serializer for validation

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,24 +2,31 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
+#User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField()
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        #extra_kwargs = {'password': {'write_only': True}}
 
+    # def create(self, validated_data):
+    #     user = User.objects.create_user(
+    #         username=validated_data['username'],
+    #         email=validated_data['email'],
+    #         password=validated_data['password']
+    #     )
+    #     return user
+    
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
-        )
-        return user
+        get_user_model().objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user 
